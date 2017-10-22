bb34f8621374a85c60f1de386105ab1d360ca061
mju <mju546mist@gmail.com>
2017-10-22 11:54:36 +0900

    Complete setting and coding list2.1

---
+++ list2.1/list2.1/list2.1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace hoge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Hello!");
+
+            Console.WriteLine(((string)list[0]).ToUpper());
+        }
+    }
+}
